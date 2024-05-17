3307ee53de699bd25e474892060bc466ad0b2ac9
missionctrlbiz <teewhy_007@yahoo.com>
2024-05-17 02:06:58 +0100

    React fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,12 @@
-//main.jsx
-
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App'; // Assuming App.jsx is in the same directory
-import './index.css'; // Add any global CSS here
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap stylesheet
-// import 'waypoints/lib/noframework.waypoints'
+import App from './App';
+import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import '@fortawesome/fontawesome-free/css/all.min.css'
-// import 'owl.carousel/dist/assets/owl.carousel.css';
-// import 'tempusdominus-bootstrap-4/build/css/tempusdominus-bootstrap-4.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-// import 'owl.carousel';
 import 'moment';
-import './styles/bootstrap.min.css'
-// import 'tempusdominus-bootstrap-4/build/js/tempusdominus-bootstrap-4.min.js';
+import './styles/bootstrap.min.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
